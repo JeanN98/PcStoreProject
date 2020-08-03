d06526a79742f877c1f4027a9fd1b65f72db66d7
PIERO <PIERO@DESKTOP-M79G0KE>
2020-08-02 23:48:35 -0500

    tymeleaf

---
+++ PcStoreProject/src/main/java/net/jpnv/PcStoreProject/PcStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package net.jpnv.PcStoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcStoreProjectApplication.class, args);
+	}
+
+}
---
+++ PcStoreProject/src/test/java/net/jpnv/PcStoreProject/PcStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jpnv.PcStoreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcStoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
