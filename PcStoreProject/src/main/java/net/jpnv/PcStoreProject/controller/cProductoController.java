8c81288c057d029aed920efceda41c9d3985be82
PIERO <PIERO@DESKTOP-M79G0KE>
2020-08-10 16:24:43 -0500

    delete

--- PcStoreProject/src/main/java/net/jpnv/PcStoreProject/controller/cProductoController.java
+++ PcStoreProject/src/main/java/net/jpnv/PcStoreProject/controller/cProductoController.java
@@ -1,14 +1,22 @@
 package net.jpnv.PcStoreProject.controller;
 
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import net.jpnv.PcStoreProject.repository.ProductoRepo;
+
 import net.jpnv.PcStoreProject.entities.cProducto; 
 
 @Controller 
@@ -28,12 +36,12 @@ public String Index() {
 
  
  @GetMapping("/add_producto")
- public String showSignUpForm(cProducto Producto) {
+ public String showSignUpForm(cProducto producto) {
      return "add_producto";
  }
  
  @GetMapping("/list")
- public String showRecipes(Model model) {
+ public String showProducts(Model model) {
 	 model.addAttribute("product", Prod.findAll());
      return "list_productos";
  }
@@ -45,7 +53,47 @@ public String Index() {
 	 model.addAttribute("product", Prod.findAll());
      return "list_productos";
  }
+ 
+ @PreAuthorize("hasAuthority('admin')")
+ @PostMapping("/add")
+ public String addProduct(cProducto producto, BindingResult result, Model model) {
+     if (result.hasErrors()) {
+        return "add_producto";
+     }    
+     Prod.save(producto);   
+     return "redirect:list";
+ }
+ 
+ @PreAuthorize("hasAuthority('admin')")
+ @GetMapping("/edit/{id}")
+ public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+     cProducto producto = Prod.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
+     model.addAttribute("product", producto);
+     return "update_producto";
+ }
   
+ 
+ @PreAuthorize("hasAuthority('admin')")
+ @PostMapping("/update/{id}")
+ public String updateRecipe(@PathVariable("id") Long id, cProducto producto, BindingResult result, Model model) {
+     if (result.hasErrors()) {
+          producto.SetId(id);
+          return "update_producto";
+     }
+
+     Prod.save(producto);
+     return "redirect:/PcStore/list";
+ }
+ 
+ 
+ @PreAuthorize("hasAuthority('admin')")
+ @GetMapping("/delete/{id}")
+ public String deleteRecipe(@PathVariable("id") Long id, Model model) {
+     cProducto producto = Prod.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid recipe Id:" + id));
+     Prod.delete(producto);	     
+     model.addAttribute("product", Prod.findAll());
+     return "list_productos";
+ }
 
  
 }
