b338b1bcbe5ee1fc3eb50f8045132ea0c04179a4
stalincopa2 <copastalin@gmail.com>
2020-08-10 06:49:43 -0500

    solucionado el problema del propertis

--- PcStoreProject/src/main/java/net/jpnv/PcStoreProject/controller/cProductoController.java
+++ PcStoreProject/src/main/java/net/jpnv/PcStoreProject/controller/cProductoController.java
@@ -1,17 +1,13 @@
 package net.jpnv.PcStoreProject.controller;
 
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+
 
 import net.jpnv.PcStoreProject.repository.ProductoRepo;
 import net.jpnv.PcStoreProject.entities.cProducto; 
--- PcStoreProject/src/main/java/net/jpnv/PcStoreProject/controller/cUsuarioController.java
+++ PcStoreProject/src/main/java/net/jpnv/PcStoreProject/controller/cUsuarioController.java
@@ -1,13 +1,13 @@
 package net.jpnv.PcStoreProject.controller;
 
-import java.util.UUID;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
